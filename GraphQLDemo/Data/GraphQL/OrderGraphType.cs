e5ebc0cad18f9fcd9b32d1179d1121e4e556f29e
jgradt <>
2019-06-08 10:19:26 -0500

    retrieve customer from OrderGraphType

--- GraphQLDemo/Data/GraphQL/OrderGraphType.cs
+++ GraphQLDemo/Data/GraphQL/OrderGraphType.cs
@@ -1,11 +1,12 @@
 ﻿using GraphQL.Types;
 using GraphQLDemo.Data.Entities;
+using GraphQLDemo.Data.Repositories;
 
 namespace GraphQLDemo.Data.GraphQL
 {
     public class OrderGraphType : ObjectGraphType<Order>
     {
-        public OrderGraphType()
+        public OrderGraphType(ICustomerRepository customerRepository)
         {
             Name = "Order";
 
@@ -15,6 +16,20 @@ namespace GraphQLDemo.Data.GraphQL
             Field<OrderStatusGraphType>(nameof(Order.Status));
             Field(x => x.TotalDue);
             Field(x => x.Comment);
+
+            FieldAsync<CustomerGraphType>(
+              "customer",
+
+              resolve: async context =>
+              {
+                  var numItems = context.GetArgument<int>("count");
+                  numItems = numItems > 0 ? numItems : 10;
+
+                  var data = await customerRepository.GetByIdAsync(context.Source.CustomerId);
+
+                  return data;
+              }
+            );
         }
     }
 
